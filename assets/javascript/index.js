44e89aabdb5d5dcacdec0d55cd58b0e040af01a1
Ben Baumann <ben@thisisbam.com>
2017-10-10 23:06:39 -0400

    updated ui.js and started index.js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,2 +1,11 @@
 game.startgame();
 ui.start();
+
+document.onkeyup = function(event){
+    if(game.lettersinsecretword.includes(event.key)){
+        console.log(ui.msg(event.key));
+    }
+    game.lettersguessed.push(event.key);
+    ui.guessesDiv.innerHTML = game.lettersguessed;
+    ui.msg(game.lettersinsecretword);
+}
--- assets/javascript/ui.js
+++ assets/javascript/ui.js
@@ -1,15 +1,22 @@
 var ui = {
-    letters : document.getElementById("letters"),
+    lettersDiv : document.getElementById("letters"),
+    guessesDiv : document.getElementById("guesses"),
+    msgDiv : document.getElementById("witty-message"),
+    hangmanDiv : document.getElementById("hangman"),
+    numguessesDiv : document.getElementById("numguesses"),
     drawWordOnScreen:function(){
-        game.lettersinsecretword.forEach(function(letter) {
+        game.lettersinsecretword.forEach((letter)=>{
             var div = document.createElement('div');
             div.innerHTML = letter;
             div.classList = "letter";
-            letters.appendChild(div);
+            this.lettersDiv.appendChild(div);
         });
-        // this.letters.innerHTML = game.lettersinsecretword;
+    },
+    msg: function(msg){
+        this.msgDiv.innerHTML = msg;
     },
     start: function(){
         this.drawWordOnScreen();
+        this.numguessesDiv.innerHTML = game.numguesses;
     }
 }
